00e0e04e3a16db65084c71fec949f3cbe3ea3950
nataliabpw <01187514@pw.edu.pl>
2026-03-09 20:42:07 +0100

    Inital Spring Boot project setup

---
+++ src/main/java/io/github/nataliabpw/maze_solver/MazeSolverApplication.java
@@ -0,0 +1,13 @@
+package io.github.nataliabpw.maze_solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazeSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nataliabpw/maze_solver/MazeSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nataliabpw.maze_solver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
