657795d6d3a62b76b390e9ea76cecfa058d81aa0
nataliabpw <01187514@pw.edu.pl>
2026-03-17 18:15:33 +0100

    Improve MazePath readability

--- src/main/java/io/github/nataliabpw/maze_solver/model/MazePath.java
+++ src/main/java/io/github/nataliabpw/maze_solver/model/MazePath.java
@@ -16,29 +16,37 @@ public class MazePath {
         int currX, currY;
         int nextX, nextY;
         for(int i=0; i < path.size()-1; i++){
-            currX = cellNumber(path.get(i) % columns);
-            currY = cellNumber(path.get(i) / columns);
-            nextX = cellNumber(path.get(i+1) % columns);
-            nextY = cellNumber(path.get(i+1) / columns);
-            if ( currX == nextX){
-                int min = Math.min(currY, nextY);
-                int max = Math.max(currY, nextY);
-                for (int j = min; j <= max; j++){
-                    if(mazeData.getCellType(currX, j) != Cell.START && mazeData.getCellType(currX, j) != Cell.END)
-                    mazeData.setCellType(type, currX, j);
-                }
-            } else{
-                int min = Math.min(currX, nextX);
-                int max = Math.max(currX, nextX);
-                for (int j = min; j <= max; j++){
-                    if(mazeData.getCellType(j, currY) != Cell.START && mazeData.getCellType(j, currY) != Cell.END)
-                    mazeData.setCellType(type, j, currY);
-                }
-            }
+            currX = getCellNumber(path.get(i) % columns);
+            currY = getCellNumber(path.get(i) / columns);
+            nextX = getCellNumber(path.get(i+1) % columns);
+            nextY = getCellNumber(path.get(i+1) / columns);
+            markLine(currX, nextX, currY, nextY, type);
         }
     }
     
-    private int cellNumber(int node){
+    private int getCellNumber(int node){
         return node * 2 + 1;
+    }
+
+    private void markLine(int x1, int x2, int y1, int y2, Cell type){
+        if ( x1 == x2){
+            int min = Math.min(y1, y2);
+            int max = Math.max(y1, y2);
+            for (int y = min; y <= max; y++){
+                setIfNotStartOrEnd(x1, y, type);
+            }
+        } else{
+            int min = Math.min(x1, x2);
+            int max = Math.max(x1, x2);
+            for (int x = min; x <= max; x++){
+                setIfNotStartOrEnd(x, y1, type);
+            }
+        }
+    }
+
+    private void setIfNotStartOrEnd(int x, int y, Cell type){
+        Cell cell = mazeData.getCellType(x, y);
+        if(cell != Cell.START && cell != Cell.END)
+                    mazeData.setCellType(type, x, y);
     }
 }
